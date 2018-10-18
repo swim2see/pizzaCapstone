f5e02192e8ad26eace86c0e28388bd067b108b60
Michael Cleary <swim2see@gmail.com>
2018-10-18 15:54:03 -0400

    Fungu?

---
+++ Assets/Good Scripts/Overworld Scripts/InteractableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+
+	public bool inventory;
+	public bool openable;
+	public bool locked;
+	public bool talks; //if true, the object can talk to the player
+
+	public GameObject itemNeed;
+	public string message;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
