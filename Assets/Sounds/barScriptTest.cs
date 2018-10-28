6b0f0862b43ef87291edab8a48c03edf8361bddd
Unknown <mad758@nyu.edu>
2018-10-28 15:45:44 -0400

    Text Mesh pro+ Bar Script

---
+++ Assets/Sounds/barScriptTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class barScriptTest : MonoBehaviour
+{
+    public float[] maxFill;
+    public float[] currentFill;
+    public Image[] theBar;
+    public bool[] isComplete;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < theBar.Length; i++)
+        {
+            theBar[i].fillAmount = currentFill[i] / maxFill[i];
+            currentFill[i] += Time.deltaTime;
+
+            if (currentFill[i] / maxFill[i] >= 1)
+            {
+                currentFill[i] = maxFill[i];
+                isComplete[i] = true;
+                print("fill complete");
+            }
+        }
+    }
+}
