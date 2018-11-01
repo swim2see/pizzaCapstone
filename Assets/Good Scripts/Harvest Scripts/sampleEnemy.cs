41a537e3afb2d86e32bd0826b6aec417ea8930a5
Unknown <mad758@nyu.edu>
2018-11-01 15:17:01 -0400

    Working on enemy running from player

---
+++ Assets/Good Scripts/Harvest Scripts/sampleEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sampleEnemy : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody2D rb;
+    public float spd;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //cam = GameObject.Find("Main Camera").GetComponent<CamControl>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+            GameObject playerObj;
+            Vector3 playerPos;
+            playerObj = GameObject.FindWithTag("Player");
+            playerPos = playerObj.transform.position;
+            Vector3 vel;
+            if (Vector2.Distance(transform.position, playerPos) > 5f)
+            {
+                vel = (transform.position-playerPos).normalized * spd / 2;
+            }
+            else
+            {
+                vel = (transform.position - playerPos).normalized * spd * 1.5f;
+            }
+            rb.MovePosition(transform.position + vel);
+        }
+    }
+}
