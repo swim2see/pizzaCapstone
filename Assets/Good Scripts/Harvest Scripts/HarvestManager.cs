df72c6dedac7679259bdb189512af09706fb2b52
Unknown <mad758@nyu.edu>
2018-11-20 10:58:27 -0500

    The enemy instantiation wasnt being turned off

--- Assets/Good Scripts/Harvest Scripts/HarvestManager.cs
+++ Assets/Good Scripts/Harvest Scripts/HarvestManager.cs
@@ -123,13 +123,16 @@ public class HarvestManager : MonoBehaviour {
             collectTimer += Time.deltaTime;
             radialTimer.fillAmount = collectTimer / totalCollectTimer;
 
-            for(int i = 0; i < 3; i++)
+            GameObject[] numberOfEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+            if (numberOfEnemies.Length < 12)
             {
-                Instantiate(enemyA, transform.position, Quaternion.identity);
-                Instantiate(enemyB, transform.position, Quaternion.identity);
-                Instantiate(enemyC, transform.position, Quaternion.identity);
+                for (int i = 0; i < 3; i++)
+                {
+                    Instantiate(enemyA, transform.position, Quaternion.identity);
+                    Instantiate(enemyB, transform.position, Quaternion.identity);
+                    Instantiate(enemyC, transform.position, Quaternion.identity);
+                }
             }
-
             if (collectTimer >= totalCollectTimer)
             {
                 gameState = 3;
