36885ceb8463a51bba1b7057aa4089b36f141442
Unknown <mad758@nyu.edu>
2018-12-04 16:42:03 -0500

    Continuing Work on makingSpells

--- Assets/Good Scripts/Harvest Scripts/makingSpells.cs
+++ Assets/Good Scripts/Harvest Scripts/makingSpells.cs
@@ -15,12 +15,73 @@ public class makingSpells : MonoBehaviour {
 		
 	}
 
-    
-    public void BreadSpell(int flavoring, int garnish)
+    //Heal Spell
+    public float BreadSpell(int flavoring, int garnish)
     {
-
+        float temp = 0;
+        if (flavoring > 0) {
+             temp = Random.Range(0f,flavoring);
+        }
+        else if(flavoring<0)
+        {
+             temp = Random.Range(flavoring, 0f);
+        }
+        switch (garnish)
+        {
+            case 1:
+                temp *= 1;
+                break;
+            case 2:
+                temp *= 2;
+                break;
+            case 3:
+                temp *= 3;
+                break;
+            case 4:
+                temp *= 4;
+                break;
+            case 5:
+                temp *= 5;
+                break;
+            default:
+                break;
+        }
+        return temp;
     }
-
+    //Damage
+    public float CheeseSpell(int flavoring, int garnish)
+    {
+        float temp = 0;
+        if (flavoring > 0)
+        {
+            temp = Random.Range(0f, flavoring);
+        }
+        else if (flavoring < 0)
+        {
+            temp = Random.Range(flavoring, 0f);
+        }
+        switch (garnish)
+        {
+            case 1:
+                temp *= 1;
+                break;
+            case 2:
+                temp *= 2;
+                break;
+            case 3:
+                temp *= 3;
+                break;
+            case 4:
+                temp *= 4;
+                break;
+            case 5:
+                temp *= 5;
+                break;
+            default:
+                break;
+        }
+        return temp;
+    }
     //Damage
     public float MeatSpell(int flavoring, int garnish) {
 
@@ -32,7 +93,48 @@ public class makingSpells : MonoBehaviour {
             case 1:
                 temp *= 1;
                 break;
+             case 2:
+                temp *= 2;
+                break;
+            case 3:
+                temp *= 3;
+                break;
+            case 4:
+                temp *= 4;
+                break;
+            case 5:
+                temp *= 5;
+                break;
+            default:
+                break;
 
+        }
+
+        return temp;
+    }
+    public float SauceSpell(int flavoring, int garnish)
+    {
+
+        //Randomizes damage inflicted, based on integer passed
+        float temp = Random.Range(0f, Mathf.Abs(flavoring));
+
+        switch (garnish)
+        {
+            case 1:
+                temp *= 1;
+                break;
+            case 2:
+                temp *= 2;
+                break;
+            case 3:
+                temp *= 3;
+                break;
+            case 4:
+                temp *= 4;
+                break;
+            case 5:
+                temp *= 5;
+                break;
             default:
                 break;
 
@@ -41,5 +143,5 @@ public class makingSpells : MonoBehaviour {
         return temp;
     }
 
-     
+
 }
